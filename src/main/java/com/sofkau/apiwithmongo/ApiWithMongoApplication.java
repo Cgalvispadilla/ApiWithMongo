dfd19c8feb20f9b296e7b915f33977a28a33107a
Cgalvispadilla <fktcg99@gmail.com>
2021-10-11 16:10:52 -0500

    se creo la clase del empleado para la coleccion de esta

---
+++ src/main/java/com/sofkau/apiwithmongo/ApiWithMongoApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.apiwithmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWithMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiWithMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sofkau/apiwithmongo/ApiWithMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.apiwithmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWithMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
